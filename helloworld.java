882cf4fc5624e1496c14d7e256941b1495e26a47
Jai0108 <70594136+Jai0108@users.noreply.github.com>
2022-06-13 11:57:42 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello Jaiydesh 13th june! webhook demo1");
+    System.out.println("Hello Jaiydesh 13th june! webhook through ngrok");
     }
 }
